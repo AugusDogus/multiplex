09f3ddcc52b6cabe29da7296268912c984420885
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2025-06-25 21:31:33 -0500

    fix: change package.json import from named to default export
    - Replace `import * as packageJson` with `import packageJson`
    - Resolves warning about named exports not being available soon
    - Maintains same functionality for accessing package version

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import * as packageJson from "~/../package.json";
+import packageJson from "~/../package.json";
 
 import { Command } from "lucide-react";
 import * as React from "react";
